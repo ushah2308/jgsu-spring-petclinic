1c7be4ef521d00f8f0bd6490c2a1f1b9ba4631b1
Urvish Shah <ushah@salesforce.com>
2022-05-05 17:21:39 -0500

    break a test 2

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("Urvish");
+		person.setFirstName("Rutvi");
 		person.setLastName("Shah");
 
 		Validator validator = createValidator();
